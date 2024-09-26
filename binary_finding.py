d6d63a7fe98dc5db68e914242282123e747a3d4a
Yugasaki <141634247+Yugasaki@users.noreply.github.com>
2024-09-26 21:06:07 +0200

    Update binary_finding.py
    workin and adding if number not exist

--- binary_finding.py
+++ binary_finding.py
@@ -1,42 +1,59 @@
-lk = int(input("what are you serching for "))
+lk = input("for what are you looking ") 
+iit = True
+try:
+    int(lk)
+except:
+    iit = False
 
-print("...")
-print("...")
-print("...")
+if (iit == True):
+    lk = int(lk)
 
-lis = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+lis = [1,2,3,4,5,6,7,8,10,11,12]
 
-L = 1
-R = int(len(lis))
-M = (L + R) // 2
+l = 0    # Left
+r = len(lis) - 1    #Right
+mid = None  #mid that we use to astablish numbers
 
-rep = True
-fir = True
 count = 0
-
-while (rep == True):
+rep = True
+done = False
+numberNotExist = False
+lr = []
+rl =[]
+oldl = None
+oldr = None
+while (rep == True and iit == True): #The iit is checked to chek it is int
 
     count += 1
+    mid = (l + r) // 2
+    rl.append(l)
+    rl.append(r)
 
-    if (lk == lis[M - 1]):
-        print("...")
-        print("...")
-        print("...")
-        print("Number " + str(lis[M - 1]) + " finded")
-        print(str(M - 1) + "th index")
-        print("done in "+ str(count) +" operations")
+    if (count > 1 and lr == rl):
+        print("this work")
+    rl.clear
+    if (lk == lis[mid]):
+        print(lis[l:r])
+        print("done in ",count," operations")
         rep = False
+        
+    elif (lk < lis[mid]):
+        r = mid - 1
+        print(lis[l:r + 1],mid + 1)
+        if (oldl == l and oldr == r):
+            print("number dont exist")
+            rep = False
+        else:
+            oldr = r
+            oldl = l
 
-    elif (lk < lis[M - 1]):
-        R = M - 1
-        print(lis[L - 1:R])
-    
-    elif (lk > lis[M - 1]):
-        L = M + 1
-        print(lis[L - 1:R])
 
-    else:
-        rep = False
-        print("error")
-
-    M = (L + R) // 2
+    elif (lk > lis[mid]):
+        l = mid + 1
+        print(mid + 1,lis[l:r + 1])
+        if (oldl == l and oldr == r):
+            print("number not exist")
+            rep = False
+        else:
+            oldr = r
+            oldl = l
